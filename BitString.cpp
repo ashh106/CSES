0ae010b83b2f87937fcf9a59733e8c7d5da13e48
Aashutosh Bharti <125398000+ashh106@users.noreply.github.com>
2025-03-20 00:47:48 +0530

    unsable to solve bit string

--- BitString.cpp
+++ BitString.cpp
@@ -1,13 +1,18 @@
 #include<bits/stdc++.h>
 #define ll long long
-#define mod 1e9 + 7 
 using namespace std;
+ll mod = 1e9 + 7 ;
 // Your task is to calculate the number of bit strings of length n.
 // For example, if n=3, the correct answer is 8, because the 
 // possible bit strings are 000, 001, 010, 011, 100, 101, 110, and 111.
 ll modPow(ll b , ll e){
 ll ans = 1;
-
+while (e){
+    if(e & 1LL) ans = (ans * b) % mod;
+    b = (b * b)% mod;
+    e >>= 1LL;
+}
+return ans;
 }
 
 int main(){

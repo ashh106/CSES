d94d2344b00c7b79afefd5312a758549cdc7176f
Aashutosh Bharti <125398000+ashh106@users.noreply.github.com>
2025-03-21 22:26:57 +0530

    verify add file

---
+++ PalindromeReorder.cpp
@@ -0,0 +1,25 @@
+/#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+const int MOD = 1000000007;
+const int INF = 1e9;
+
+void solve() {
+    // Write your code here
+}
+
+int main() {
+    fastio;
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
